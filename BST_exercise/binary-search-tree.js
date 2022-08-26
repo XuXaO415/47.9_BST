8abfec1712dcddc50c4b6bf1a2a7b9e1f94ed60c
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-25 18:08:05 -0700

    Added instances for insert

--- BST_exercise/binary-search-tree.js
+++ BST_exercise/binary-search-tree.js
@@ -11,12 +11,54 @@ class BinarySearchTree {
     this.root = root;
   }
 
+
+  inOrderTraversal(node = this.root) {
+    if (node.left) inOrderTraversal(node.left);
+    console.log(node.val);
+    if (node.right) inOrderTraversal(node.right);
+  }
+
+  preOrderTraversal(node = this.root) {
+    console.log(node.val);
+    if (node.left) preOrderTraversal(node.left);
+    if (node.right) preOrderTraversal(node.right);
+  }
+
+  postOrderTraversal(node = this.root) {
+    if (node.left) postOrderTraversal(node.left);
+    if (node.right) postOrderTraversal(node.right);
+    console.log(node.val);
+  }
+
+
   /** insert(val): insert a new node into the BST with value val.
    * Returns the tree. Uses iteration. */
 
+  //This function should insert a node in a binary tree. It should return the BinarySearchTree and should be solved using iteration.
+
   insert(val) {
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
 
-
+    let currentNode = this.root;
+    while (currentNode) {
+      if (val < currentNode.val) { // if val is less than currentNode.val, go left
+        if (!currentNode.left) { // if there is no left child 
+          currentNode.left = new Node(val); // make a new node and set it to currentNode.left
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) { // if there is no right child
+          currentNode.right = new Node(val); // make a new node and set it to currentNode.right
+          // console.log(`Node ${val} was inserted into the binary tree`);
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
@@ -93,3 +135,22 @@ class BinarySearchTree {
 }
 
 module.exports = BinarySearchTree;
+
+binarySearchTree = new BinarySearchTree();
+binarySearchTree.insert(15);
+binarySearchTree.insert(20);
+binarySearchTree.insert(10);
+binarySearchTree.insert(12);
+binarySearchTree.root.value // 15
+binarySearchTree.root.right.value // 20
+binarySearchTree.root.left.right.value // 12
+
+
+binarySearchTree = new BinarySearchTree();
+binarySearchTree.insert(15)
+binarySearchTree.insert(20)
+binarySearchTree.insert(10)
+binarySearchTree.insert(12);
+binarySearchTree.root.value // 15
+binarySearchTree.root.right.value // 20
+binarySearchTree.root.left.right.value // 12
